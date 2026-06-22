21e292854ffc8385e9d9fef2d694a927a1272c29
navreet111 <navreet1.17@gmail.com>
2026-06-22 11:05:33 +0530

    Review 5 codes

---
+++ oops/inheritance.java
@@ -0,0 +1,25 @@
+class Animal {
+
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+
+class Dog extends Animal {
+
+    void bark() {
+        System.out.println("Dog is barking");
+    }
+}
+
+public class inheritance
+ {
+
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+
+        d.eat();   // inherited method
+        d.bark();
+    }
+}
